2964b8d1705d9a228a3c93317cedc29c4ad9833f
Cathy Hu <cathy.hu@suse.com>
2026-02-04 17:42:23 +0100

    qga: Add selinux-helper for guest-exec subcommand (bsc#1237450)
    Problem:
    
    ATM the QEMU Guest Agent and SELinux are not working together properly.
    The fedora (and therefor also the openSUSE) policy confine the qemu-guest-agent
    service in the domain `qemu_ga_t`. That means, qemu-guest-agent
    is only allowed to do what the policy says.
    
    However, the `guest-exec` command allows arbitrary execution
    of code from a privileged service, which conflicts with the
    notion of SELinux confinement.
    
    ATM, the policy allows only some accesses that are used
    by other qemu-guest-agent commands.
    That means, the qemu-guest-agent fails sporadically, depending
    on what is allowed for other commands.
    However, `guest-exec` would need to allow everything.
    
    see https://bugzilla.suse.com/show_bug.cgi?id=1237450
    
    Solution:
    
    This is not an great solution, but it works like this:
    We add a "wrapper" which is executed instead of the program
    that is called via `guest-exec`. The "wrapper" just
    re-executes the command given by `guest-exec`.
    This way, on the SELinux policy side we can give that
    wrapper executable a label on the file system.
    With that label, we can transition into a more broader
    unconfined domain _and_ toggle that transition with a
    SELinux boolean. That would make `guest-exec`
    consistently allowed to execute or not by policy.
    
    This needs a change on the SELinux policy side to
    accompany this with.
    
    What other options have been tried unsuccessfully:
    
    - Fixing via SELinux policy: It is not possible for
      one domain to have different permissions depending on
      code path. It is also not possible to toggle the permissive
      state via a SELinux boolean, so users would need
      to add it via semanage.
    - Setting the domain of the executed commands directly
      to a broader domain with setcon/setexeccon.
      The SELinux kernel does not allow to spawn a process
      directly with those that has broader privileges than the parent.
    
    What other options are there to solve this issue:
    
    - Making the qemu-guest-agent unconfined by default
    - Document the workaround to use semanage to make the domain permissive
      if `exec-guest` is needed as works as intended and ignore the problem

---
+++ qga/qemu-ga-selinux-helper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <glib.h>
+
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        return EXIT_FAILURE;
+    }
+
+    execvp(argv[1], argv + 1);
+
+    int err = errno;
+    fprintf(stderr, "%s: %s\n", argv[1], strerror(err));
+
+    exit(EXIT_FAILURE);
+}
